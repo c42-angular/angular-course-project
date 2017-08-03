9b85f473c77a91473f5c6e00d577d2fbadae4317
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2017-08-03 06:48:37 +0100

    Section 6 end: primitive version of shopping list able to add items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputElement: ElementRef;
+  @ViewChild('amountInput') amountInputElement: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  OnAdd() {
+    console.log('OnAdd() called');
+    const ingdtName = (<HTMLInputElement>this.nameInputElement.nativeElement).value;
+    const ingdtAmount = Number((<HTMLInputElement>this.amountInputElement.nativeElement).value);
+    const ingredient =  new Ingredient(ingdtName, ingdtAmount);
+    this.ingredientAdded.emit(ingredient);
+    console.log('Name: %s; Amount: %d', ingredient.name, ingredient.amount);
+  }
 }
