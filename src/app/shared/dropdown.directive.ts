d72dea4507ead25a3fb0ba8fa0e1b9b8a6fd497c
arun@win10.bootcamp.com <arun@win10.bootcamp.com>
2017-08-21 12:30:19 +0100

    add/remove class on an element using directive - improvement on previous version

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,20 +1,14 @@
-import { Directive, HostListener, ElementRef } from "@angular/core";
+import { Directive, HostListener, ElementRef, HostBinding } from "@angular/core";
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirective {
-    private isOpen = false;
+    @HostBinding('class.open') isOpen = false;
 
     constructor(private theElement: ElementRef){}
     
     @HostListener('click') toggleOpen(event: Event){
-        if (!this.isOpen){
-            this.theElement.nativeElement.classList.add('open');
-        } else {
-            this.theElement.nativeElement.classList.remove('open');
-        }
-
         this.isOpen = !this.isOpen;
     }
 }
