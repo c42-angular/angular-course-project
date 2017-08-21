52e26259336a8c31bc9e9088d107a6e864532113
arun@win10.bootcamp.com <arun@win10.bootcamp.com>
2017-08-21 12:01:50 +0100

    Recipes and shopping list components interact within themselves via recipe & shoppinglist services respectively

--- src/app/recipes/services/recipe-service.service.ts
+++ src/app/recipes/services/recipe-service.service.ts
@@ -1,14 +1,28 @@
 import { Recipe } from "../recipe.model";
 import { Output, EventEmitter } from "@angular/core";
+import { Ingredient } from "../../shared/ingredient.model";
 
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'Recipe description goes here',
+        new Recipe(
+            'A test recipe', 
+            'Recipe description goes here',
           'http://www.seriouseats.com/recipes/assets_c/2015/12/20151218-braised-' +
-            'chicken-thigh-cabbage-pancetta-recipe-kenji-14-thumb-1500xauto-428923.jpg'),
-        new Recipe('A test recipe 2', 'Recipe description 2 goes here',
+            'chicken-thigh-cabbage-pancetta-recipe-kenji-14-thumb-1500xauto-428923.jpg',
+            [
+                new Ingredient('Cinnamon', 5),
+                new Ingredient('Garlic', 10)
+              ]
+        ),
+        new Recipe(
+            'A test recipe 2', 
+            'Recipe description 2 goes here',
           'https://www.bbcgoodfood.com/sites/default/files/styles/top_recipe_collection/' +
-          'public/recipe-collections/collection-image/2013/05/rack-of-venison-roasted-carrots-and-forager-sauce.jpg?itok=vVcfEX9K')
+          'public/recipe-collections/collection-image/2013/05/rack-of-venison-roasted-carrots-and-forager-sauce.jpg?itok=vVcfEX9K',
+          [
+            new Ingredient('Carrots', 5),
+            new Ingredient('Meat', 1)
+          ])
       ];
 
       @Output() recipeSelected = new EventEmitter<Recipe>();
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -21,8 +21,4 @@ export class ShoppingListComponent implements OnInit {
       }
     )
   }
-
-  onIngredientAdded(ingredient: Ingredient) {
-    this.shoppingListService.addIngredient(ingredient);
-  }
 }
