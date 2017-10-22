f81ab5d3dd2c4e6cf024dd0a7ac06f50c7517b4e
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2017-10-22 06:34:27 +0100

    Recipe Edit form implemented using Reactive approach.  TODO: Save()

--- src/app/recipes/services/recipe-service.service.ts
+++ src/app/recipes/services/recipe-service.service.ts
@@ -1,13 +1,14 @@
-import { Recipe } from "../recipe.model";
-import { Output, EventEmitter, Injectable } from "@angular/core";
-import { Ingredient } from "../../shared/ingredient.model";
-import { ShoppingListService } from "../../shopping-list/services/shopping-list-service.service";
+import { Output, EventEmitter, Injectable } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import { Recipe } from '../recipe.model';
+import { ShoppingListService } from '../../shopping-list/services/shopping-list-service.service';
 
 @Injectable()
 export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe(
-            'A test recipe', 
+            'A test recipe',
             'Recipe description goes here',
           'http://www.seriouseats.com/recipes/assets_c/2015/12/20151218-braised-' +
             'chicken-thigh-cabbage-pancetta-recipe-kenji-14-thumb-1500xauto-428923.jpg',
@@ -17,7 +18,7 @@ export class RecipeService {
               ]
         ),
         new Recipe(
-            'A test recipe 2', 
+            'A test recipe 2',
             'Recipe description 2 goes here',
           'https://www.bbcgoodfood.com/sites/default/files/styles/top_recipe_collection/' +
           'public/recipe-collections/collection-image/2013/05/rack-of-venison-roasted-carrots-and-forager-sauce.jpg?itok=vVcfEX9K',
@@ -32,9 +33,13 @@ export class RecipeService {
       constructor(private shoppingListService: ShoppingListService) {
       }
 
-      getRecipes(){
+      getRecipes() {
           // return a copy of the array to keep data secure
           return this.recipes.slice();
+      }
+
+      getRecipe(index: number) {
+        return this.recipes[index];
       }
 
       onRecipeSelected(recipe: Recipe) {
