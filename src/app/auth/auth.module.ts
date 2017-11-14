42873fcefd00f29539cfee956d1a80872024fe05
arun@win10.bootcamp.com <arun@win10.bootcamp.com>
2017-11-14 08:03:35 +0000

    Auth routing module added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     FormsModule,
-    SharedModule
+    AuthRoutingModule
   ]
 })
 export class AuthModule {
