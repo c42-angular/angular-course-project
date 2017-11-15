b4a1be52a9be3b4b9c14eecac4412b4d0518952a
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2017-11-15 18:56:23 +0000

    [providers] moved from app.module to core.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/services/shopping-list-service.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth.guard';
-import { RecipeService } from './recipes/services/recipe-service.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -26,7 +21,6 @@ import { CoreModule } from './core/core.module';
     SharedModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { ShoppingListService } from '../shopping-list/services/shopping-list-service.service';
+import { RecipeService } from '../recipes/services/recipe-service.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,8 @@ import { AppRoutingModule } from '../app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent
-  ]
+  ],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
 })
 export class CoreModule {
 }
